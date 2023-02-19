2d9228821bcf4c10d23fe99e11a5744fb6a29b21
HoJo42 <howardwalz@gmail.com>
2023-02-18 17:31:55 -0800

    initial commit, drivetrain should work

---
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -0,0 +1,45 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.Constants;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.Drive;
+
+public class DriveCommand extends CommandBase {
+  private final Drive m_Drive;
+  private final CommandXboxController m_dStick;
+
+  /** Creates a new DriveCommand. */
+  public DriveCommand(Drive drive, CommandXboxController dStick) {
+    // Use addRequirements() here to declare subsystem dependencies.
+
+    m_Drive = drive;
+    m_dStick = dStick;
+
+    addRequirements(drive);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_Drive.drive(m_dStick.getLeftY() * Constants.DriveTrain.SPEED_CONTROL, m_dStick.getLeftX() * Constants.DriveTrain.SPEED_CONTROL);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -0,0 +1,22 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Drive extends SubsystemBase {
+  DifferentialDrive diffDrive;
+
+  /** Creates a new Drive. */
+  public Drive(MotorControllerGroup leftDriveMotors, MotorControllerGroup rightDriveMotors) {
+    diffDrive = new DifferentialDrive(leftDriveMotors, rightDriveMotors);
+  }
+
+  public void drive(double frontBack, double leftRight){
+    diffDrive.arcadeDrive(frontBack,leftRight);
+  }
+}
